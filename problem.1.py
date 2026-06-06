c525d284b957bcf1318882408baa5a0ee658e468
GAUTAM GUPTA <gautamguptamit@gmail.com>
2026-06-06 11:18:58 +0530

    dddd

---
+++ problem.1.py
@@ -0,0 +1,10 @@
+# print('''
+#        asldasjkd
+#        dlhaskdjask
+#        sdjasdljasokd
+#        ''')
+import os
+directory_path = '/JAI BADRI VISHAL'
+contents = os.listdir(directory_path)
+for item in contents:
+       print(item)
